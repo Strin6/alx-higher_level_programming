15fcab3bf0572b8cd96199d090d0fa563c6bfb3e
Strin6 <esebaby50@gmail.com>
2023-11-17 06:10:46 -0800

    sorted dictionary

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,6 +2,5 @@
 def print_sorted_dictionary(a_dictionary):
     sorted_keys = sorted(a_dictionary.keys())
 
-    # Iterate through the sorted keys and print key-value pairs
     for key in sorted_keys:
-        print(f'{key}: {a_dictionary[key]}')
+        print(f'{key} : {a_dictionary[key]}')
