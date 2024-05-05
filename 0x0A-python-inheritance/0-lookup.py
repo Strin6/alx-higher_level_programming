69995cf0871d3aa2e2b7e7825ca419bafacbff24
isaac okechukwu chukwuma <isaacokechukwu200021@gmail.com>
2024-05-05 23:59:55 +0100

    done

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -2,9 +2,5 @@
 
 def lookup(obj):
     # Get all attributes and methods of the object
-    all_attributes_and_methods = dir(obj)
+    return dir(obj);
 
-    # Filter out the attributes and methods that start with underscores
-    filtered_attributes_and_methods = [attr for attr in all_attributes_and_methods if not attr.startswith('_')]
-
-    return filtered_attributes_and_methods
